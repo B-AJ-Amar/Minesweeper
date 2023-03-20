732b57235b2fd67891229eb23d77f99fea8933fa
Bkz-Amar <amarbouakaz91@gmail.com>
2023-03-20 08:32:45 +0100

    add the header file

---
+++ header.py
@@ -0,0 +1,9 @@
+from PyQt6.QtWidgets import *
+from PyQt6.QtGui  import *
+from PyQt6.QtCore import *
+from PyQt6 import *
+import random
+from random import *
+from math import *
+from main import *
+
